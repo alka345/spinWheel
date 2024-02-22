b11dfa98fd7bc9735d562478fece0109d67c4ec7
alka345 <alkaraojnv@gmail.com>
2024-02-22 18:27:06 +0530

    Pop Up

--- spin.js
+++ spin.js
@@ -1,6 +1,18 @@
+let openBtn = document.querySelector('.btn');
+let closeBtn = document.querySelector('.close')
 let wheel = document.querySelector('.wheel');
 let spinBtn = document.querySelector('.spinBtn');
 let value = Math.floor(Math.random() * 3600);
+let popup = document.getElementById("popup");
+
+
+openBtn.onclick= function(){
+    popup.classList.add("open-popup")
+}
+
+closeBtn.onclick = function(){
+    popup.classList.remove("open-popup")  
+}
 
 
 spinBtn.onclick = function(){
